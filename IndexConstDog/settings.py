8c13a63d92254602c3eefb960790a30f28ed0973
xmduhan <xmduhan@gmail.com>
2016-03-03 16:58:48 +0800

    ...

--- IndexConstDog/settings.py
+++ IndexConstDog/settings.py
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'IndexConstDog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-# """
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
