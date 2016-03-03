968ffdd5e20760a6eb2d2a35183c0f67d798dbf6
xmduhan <xmduhan@gmail.com>
2016-03-03 16:13:27 +0800

    ...

--- IndexConstDog/settings.py
+++ IndexConstDog/settings.py
@@ -56,13 +56,25 @@ WSGI_APPLICATION = 'IndexConstDog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'indexconst',
+        'USER': 'indexconst',
+        #'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
